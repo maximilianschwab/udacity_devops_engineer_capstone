3f5cea7cd90e8c35a7aac364e1fa7c3f9cd49a25
Maximilian Schwab <maximilian.schwab@gmail.com>
2020-10-25 18:49:32 +0100

    feat - added script for testing flask api

---
+++ app/post_greeting_name.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+PORT=8080
+echo "Port: $PORT"
+
+# POST method predict
+curl -d '{
+    "number1":1,
+    "number2":2
+}'\
+     -H "Content-Type: application/json" \
+     -X POST http://localhost:$PORT/helloworld
