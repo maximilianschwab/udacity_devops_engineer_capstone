5fa9557eb5cfa325eb61bce5ff9f0d502f4f59ad
Maximilian Schwab <maximilian.schwab@gmail.com>
2020-10-28 18:34:53 +0100

    chore - added capabilites parameter

--- aws/add_user.sh
+++ aws/add_user.sh
@@ -2,3 +2,4 @@ aws cloudformation create-stack \
 --stack-name "capstone-project-user-credentials" \
 --template-body file://credentials.yaml \
 --region=us-west-2 \
+--capabilities CAPABILITY_NAMED_IAM \
